059a2e572f509500dbadb60e755772737a9f40a4
Vinicius L. Diniz <42078940+viniciusldiniz@users.noreply.github.com>
2025-01-19 20:35:32 -0300

    Update Script.js

--- Assets/js/Script.js
+++ Assets/js/Script.js
@@ -1,4 +1,6 @@
-// Obtenha o elemento do toggle de modo escuro
+// jscript.js
+
+// Script de Modo Escuro
 const darkModeToggle = document.getElementById('darkModeToggle');
 const body = document.body;
 
@@ -21,3 +23,15 @@ darkModeToggle.addEventListener('change', () => {
         localStorage.setItem('darkMode', 'disabled');
     }
 });
+
+// Script para traduzir
+function translateLanguage(language) {
+    var googleTranslateElement = new google.translate.TranslateElement({
+        pageLanguage: 'pt',
+        includedLanguages: 'en,es,fr,de',
+        layout: google.translate.TranslateElement.InlineLayout.SIMPLE
+    }, 'google_translate_element');
+    
+    // A tradução será ativada conforme a bandeira clicada
+    googleTranslateElement.showLanguage(language);
+}
